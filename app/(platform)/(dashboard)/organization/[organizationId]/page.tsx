58308a21eb383b054d83b3f48c3820cd3ed1f3dd
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-11-16 11:25:51 +0530

    Add Navbar and DashboardLayout components

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,12 @@
+import { OrganizationSwitcher } from "@clerk/nextjs";
+import React from "react";
+
+const OrganizationIdPage = () => {
+  return (
+    <div>
+      <OrganizationSwitcher hidePersonal />
+    </div>
+  );
+};
+
+export default OrganizationIdPage;
