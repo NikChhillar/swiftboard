5a36c1ca1bba59fee63d22b274dfedc8e48b883c
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-11-17 10:49:01 +0530

    sidebar ⭕

--- app/(platform)/(dashboard)/_components/Sidebar.tsx
+++ app/(platform)/(dashboard)/_components/Sidebar.tsx
@@ -1,6 +1,45 @@
 "use client";
 
-const Sidebar = () => {
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+import { useLocalStorage } from "usehooks-ts";
+
+interface SidebarProps {
+  storageKey?: string;
+}
+
+const Sidebar = ({ storageKey = "s-sidebar-state" }: SidebarProps) => {
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {}
+  );
+
+  const { organization: activeOrganization, isLoaded: isLoadedOrg } =
+    useOrganization();
+
+  const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    },
+  });
+
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
+
+      return acc;
+    },
+    []
+  );
+
+  const onExpand = (id: string) => {
+    setExpanded((curr) => ({
+      ...curr,
+      [id]: !expanded[id],
+    }));
+  };
+
   return <div>Sidebar</div>;
 };
 
