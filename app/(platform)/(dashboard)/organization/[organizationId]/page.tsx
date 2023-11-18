ff344a32541370533ef954841d6affc47d8d3ca5
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-11-18 12:22:52 +0530

    Add create board functionality and display boards on organization page

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,10 +1,26 @@
-import { OrganizationSwitcher } from "@clerk/nextjs";
-import React from "react";
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
 
-const OrganizationIdPage = () => {
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+
   return (
-    <div>
-      <OrganizationSwitcher hidePersonal />
+    <div className="flex flex-col space-y-4">
+      <form action={create}>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          placeholder="Name here"
+          className="p-2 border border-black mx-2"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+      {boards.map((board) => (
+        <div key={board.id}> Name: {board.title}</div>
+      ))}
     </div>
   );
 };
