b56e2af3b3af4d6dfdef3d446d27dd1c75444de8
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-11-25 14:27:21 +0530

    Refactor form component and update create action

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,21 +1,33 @@
 "use client";
 
-import { create } from "@/actions/create-board";
+import { createBoard } from "@/actions/create-board";
 import { Button } from "@/components/ui/button";
-import React from "react";
-import { useFormState } from "react-dom";
 import { FormInput } from "./input";
+import { useAction } from "@/hooks/use-action";
 
 const FormCreate = () => {
-  const initialState = { message: '', errors: {} };
-  const [state, dispatch] = useFormState(create, initialState);
+
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess(data) {
+      console.log(data);
+    },
+    onError(error) {
+      console.error(error)
+    },
+  })
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get('title') as string
+
+    execute({ title })
+  }
 
   return (
-    <form action={dispatch}>
-      <FormInput errors={state?.errors} />
+    <form action={onSubmit}>
+      <FormInput errors={fieldErrors} />
       <Button className="my-2" type="submit">Submit</Button>
     </form>
   );
 };
-
+// 4.15
 export default FormCreate;
