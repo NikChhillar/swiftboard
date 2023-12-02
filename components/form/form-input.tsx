42bfd98b0b1013c0353c6214fb2ca202bc10782d
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-12-02 15:06:15 +0530

    Update form input component and add label component

--- components/form/form-input.tsx
+++ components/form/form-input.tsx
@@ -2,14 +2,17 @@
 
 import { forwardRef } from "react";
 import { useFormStatus } from "react-dom";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { cn } from "@/lib/utils";
 
 interface FormInputProps {
   id: string;
   label?: string;
   type?: string;
   placeholder?: string;
-  required?: string;
-  disabled?: string;
+  required?: boolean;
+  disabled?: boolean;
   errors?: Record<string, string[] | undefined>;
   className?: string;
   defaultValue?: string;
@@ -34,7 +37,33 @@ export const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
   ) => {
     const { pending } = useFormStatus();
 
-    return <div></div>;
+    return (
+      <div className="space-y-2">
+        <div className="space-y-1">
+          {label ? (
+            <Label
+              htmlFor={id}
+              className="text-xs font-semibold text-neutral-700"
+            >
+              {label}
+            </Label>
+          ) : null}
+          <Input
+            onBlur={onBlur}
+            defaultValue={defaultValue}
+            ref={ref}
+            required={required}
+            name={id}
+            id={id}
+            placeholder={placeholder}
+            type={type}
+            disabled={pending || disabled}
+            className={cn("text-sm px-2 py-1 h-7", className)}
+            aria-describedby={`${id}-error`}
+          />
+        </div>
+      </div>
+    );
   }
 );
 
