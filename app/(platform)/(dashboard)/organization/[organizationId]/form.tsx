6d4f432035f6ac8df2540c7cf5c5d84eab038cd2
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-12-05 19:17:34 +0530

    Add form submit component

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -2,34 +2,36 @@
 
 import { createBoard } from "@/actions/create-board";
 import { FormInput } from "@/components/form/form-input";
+import FormSubmit from "@/components/form/form-submit";
 import { Button } from "@/components/ui/button";
 import { useAction } from "@/hooks/use-action";
 
 const FormCreate = () => {
-
   const { execute, fieldErrors } = useAction(createBoard, {
     onSuccess(data) {
       console.log(data);
     },
     onError(error) {
-      console.error(error)
+      console.error(error);
     },
-  })
+  });
 
   const onSubmit = (formData: FormData) => {
-    const title = formData.get('title') as string
+    const title = formData.get("title") as string;
 
-    execute({ title })
-  }
+    // console.log({ title });
+
+    execute({ title });
+  };
 
   return (
     <form action={onSubmit}>
       <div className="flex flex-col space-y-2">
-        <FormInput id="title" errors={fieldErrors} />
+        <FormInput id="title" label="Board title" errors={fieldErrors} />
       </div>
-      <Button className="my-2" type="submit">Submit</Button>
+      <FormSubmit>Save</FormSubmit>
     </form>
   );
 };
-// 4.15
+// 4.31
 export default FormCreate;
