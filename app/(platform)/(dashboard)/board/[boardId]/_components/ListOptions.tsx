61545521b28f5c34f79c252d2ebbca675d06ff36
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2024-01-01 16:27:06 +0530

    Add copyList action and schema

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListOptions.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListOptions.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { copyList } from "@/actions/copy-list";
+import { deleteList } from "@/actions/delete-list";
 import FormSubmit from "@/components/form/form-submit";
 import { Button } from "@/components/ui/button";
 import {
@@ -9,16 +11,56 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { Separator } from "@/components/ui/separator";
+import { useAction } from "@/hooks/use-action";
 import { List } from "@prisma/client";
 import { MoreHorizontal, X } from "lucide-react";
+import { ElementRef, useRef } from "react";
+import { toast } from "sonner";
 
 interface ListOptionsProps {
   data: List;
-
   onAddCard: () => void;
 }
 
 const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
+  //
+
+  const closeRef = useRef<ElementRef<"button">>(null);
+
+  const { execute: executeDelete } = useAction(deleteList, {
+    onSuccess: (data) => {
+      toast.success("List deleted successfully...");
+      closeRef.current?.click();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const { execute: executeCopy } = useAction(copyList, {
+    onSuccess: (data) => {
+      toast.success("List copied successfully...");
+      closeRef.current?.click();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const onDelete = (formData: FormData) => {
+    const id = formData.get("id") as string;
+    const boardId = formData.get("boardId") as string;
+
+    executeDelete({ id, boardId });
+  };
+
+  const onCopy = (formData: FormData) => {
+    const id = formData.get("id") as string;
+    const boardId = formData.get("boardId") as string;
+
+    executeCopy({ id, boardId });
+  };
+
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -30,7 +72,7 @@ const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
         <div className="text-sm font-medium text-center text-neutral-600 pb-4">
           List actions
         </div>
-        <PopoverClose asChild>
+        <PopoverClose ref={closeRef} asChild>
           <Button
             className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
             variant="ghost"
@@ -46,7 +88,7 @@ const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
           Add a card...
         </Button>
 
-        <form action="">
+        <form action={onCopy}>
           <input hidden name="id" id="id" value={data.id} />
           <input hidden name="boardId" id="boardId" value={data.boardId} />
           <FormSubmit
@@ -59,8 +101,7 @@ const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
 
         <Separator />
 
-
-        <form action="">
+        <form action={onDelete}>
           <input hidden name="id" id="id" value={data.id} />
           <input hidden name="boardId" id="boardId" value={data.boardId} />
           <FormSubmit
