725897a59ff5d0006757e9df937d16a331723278
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2024-01-02 15:29:00 +0530

    Add create-card functionality and server actions

---
+++ actions/create-card/schema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const CreateCard = z.object({
+  title: z
+    .string({
+      required_error: "Title is required..",
+      invalid_type_error: "Title is req...",
+    })
+    .min(3, {
+      message: "Title length should be greater than 2 letters",
+    }),
+  boardId: z.string(),
+  listId: z.string(),
+});
