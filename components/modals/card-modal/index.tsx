067c3024a077a3a3fbc4bf2bb93bd7fbd2c52f1c
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2024-01-05 11:38:34 +0530

    Add Description component to CardModal

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -6,6 +6,7 @@ import { fetcher } from "@/lib/fetcher";
 import { CardWithList } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 import Header from "./header";
+import Description from "./description";
 
 export const CardModal = () => {
   //
@@ -25,7 +26,13 @@ export const CardModal = () => {
         {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
         <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
           <div className="col-span-3">
-            <div className="w-full space-y-6"></div>
+            <div className="w-full space-y-6">
+              {!cardData ? (
+                <Description.Skeleton />
+              ) : (
+                <Description data={cardData} />
+              )}
+            </div>
           </div>
         </div>
       </DialogContent>
